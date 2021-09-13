b4b674441ffb16d19b1d64dbede905e735c1844b
ZoeyYingying <Zoey13652755443@gmail.com>
2021-09-13 02:40:07 -0600

    Initial commit

---
+++ CoronavirusTracker/src/test/java/com/zoey/coronavirustracker/CoronavirusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoey.coronavirustracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
